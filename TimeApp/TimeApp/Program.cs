ffe12fb4663fc82a9dfacc0528bdac1925cbede6
moudroff <moudroff>
2020-09-04 14:29:21 +0300

    Time

---
+++ TimeApp/TimeApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Time t1, t2, t3;
+            t1 = Time.CreateFromTick(100);
+            t2 = Time.CreateFromSec(100);
+            t3 = Time.CreateFromMin(100);
+            Console.WriteLine(t1);
+            Console.WriteLine(t2);
+            Console.WriteLine(t3);
+
+            Console.WriteLine(t1 + t2);
+            Console.WriteLine(t3 - t2);
+            Console.WriteLine(t1 - t2);
+
+
+
+
+        }
+    }
+}
---
+++ TimeApp/TimeApp/Time.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeApp
+{
+    class Time
+    {
+        private readonly decimal tick;
+
+        public decimal Tick 
+        {
+            get => tick;            
+        }
+         
+        public decimal Sec
+        {
+            get => tick / 18.2m;
+        }
+        public decimal Min
+        {
+            get => tick / 18.2m / 60;
+        }
+
+        private Time(decimal t)
+        {
+            tick = t;
+        }
+
+        static public Time CreateFromTick(decimal t)
+        {
+            return new Time(t);
+        }
+
+        static public Time CreateFromSec(decimal t)
+        {
+            return new Time(t * 18.2m);
+        }
+        static public Time CreateFromMin(decimal t)
+        {
+            return new Time(t * 60 * 18.2m);
+        }
+
+        static public Time operator+(Time t1, Time t2)
+        {
+            return new Time(t1.Tick + t2.Tick);
+        }
+
+        static public Time operator-(Time t1, Time t2)
+        {
+            return new Time(t1.Tick - t2.Tick);
+        }
+                                    
+        public override string ToString()
+        {
+            return $"tick-{Tick:0.00}   sec " + 
+                $"- {Sec:0.00}  min - {Min:0.00}  ";
+        }
+
+
+
+    }
+}
