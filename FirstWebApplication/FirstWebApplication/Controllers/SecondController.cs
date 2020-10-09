f2531bae8faef5e8205a30bfaba7c6c47998e3f5
moudroff <moudroff>
2020-10-09 13:22:22 +0300

    Second

---
+++ FirstWebApplication/FirstWebApplication/Controllers/SecondController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebApplication.Controllers
+{
+    public class SecondController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult MapPage()
+        {
+            
+            ViewBag.ID = Request.Query["id"];
+            ViewData["Name"] = Request.Query["name"];
+
+
+            ViewData["Session"] = Request.HttpContext.Session.ToString();
+
+
+            return View();
+        }
+    }
+}
